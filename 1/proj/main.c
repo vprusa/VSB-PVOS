0617b141731c1b7b818de89657860e3f13589a38
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-10-04 21:59:50 +0200

    init ukol 1

---
+++ 1/proj/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+Vytvořte si proces s N potomky, kdy potomci se náhodně ukončí různým způsobem. Např. korektně i nekorektně: neplatný pointer, dělení 0. Proveďte exec, např. programu ls.
+*/
+void first() {
+
+}
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
