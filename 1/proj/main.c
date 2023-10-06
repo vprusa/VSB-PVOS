234348788bd922ea1d2f5114613abfa4120343ad
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-10-07 00:12:25 +0200

    ukol bkp

--- 1/proj/main.c
+++ 1/proj/main.c
@@ -13,6 +13,7 @@
 Vytvořte si proces s N potomky, kdy potomci se náhodně ukončí různým způsobem. Např. korektně i nekorektně: neplatný pointer, dělení 0. Proveďte exec, např. programu ls.
 */
 void first(int N) {
+  printf("first\n");
   pid_t child_pid, wpid;
   int status = 0;
 
@@ -62,6 +63,7 @@ void first(int N) {
   
   printf("waiting: pid: %i, rand: %i\n", pid_id, pid);
   while ((wpid = wait(&status)) > 0); // this way, the father waits for all the child processes 
+  printf("first - done\n");
 }
 
 /**
@@ -69,7 +71,10 @@ Rodič si bude stále udržovat N potomků. N bude argument programu.
 
 Myšlenka:
 */
-void second() {
+void second(int N) {
+  printf("second\n");
+  int T = 1;
+  int Tu = 100000;
   /**
   while () {
     if ( fork() == 0 ) { sleep( rand() % T ); return 0; }
@@ -81,12 +86,92 @@ void second() {
   /*
    * Rodič bude opět rozpoznávat způsob ukončení potomka a povede si statistiku, kolik procesů vytvořil, kolik jich skončilo a cca každou vteřinu vypíše stav.
   */
+
+  pid_t child_pid, wpid;
+  int status = 0;
+
+  pid_t pid = 0;
+  int pid_id = 0;
+/*  for (int i = 1; i <= N; i++) {
+    if(pid == 0) {
+      pid = fork();
+      pid_id = i;
+    }
+
+    if ( fork() == 0 ) { 
+      sleep( rand() % T ); return 0; 
+    }
+    usleep( Tu );
+    // while waitpid( ... NOHANG ).... // posbirá jen ukončené potomky
+    while ((wpid = wait(&status)) > 0);
+  }*/
+
+  printf("waiting: pid: %i, rand: %i\n", pid_id, pid);
+  // while ((wpid = wait(&status)) > 0); // this way, the father waits for all the child processes 
+  printf("second - done\n");
+}
+
+int second2(int NUM_PROCESSES) {
+    int MAX_TIME = 1;
+    int pid, status;
+    int running_time[NUM_PROCESSES];
+    int finish_state[NUM_PROCESSES];
+    int num_finished = 0;
+    int num_exceptions = 0;
+
+    // Spawn child processes
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        pid = fork();
+        if (pid == 0) {
+            // Child process
+//            srand(time(NULL));
+            running_time[i] = rand() % MAX_TIME + 1;
+            printf("Process %d started\n", i);
+            printf("Running time: %d ms\n", running_time[i]);
+            sleep(running_time[i]);
+            if (rand()  % 2 == 0) {
+                printf("Process %d finished successfully\n", i);
+                finish_state[i] = 0;
+            } else {
+                printf("Process %d ended with exception\n", i);
+                finish_state[i] = 1;
+                num_exceptions++;
+            }
+            exit(0);
+        } else if (pid < 0) {
+            // Error spawning process
+            printf("Error spawning process %d\n", i);
+            exit(1);
+        }
+    }
+
+    // Wait for child processes to finish
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status)) {
+            num_finished++;
+        }
+    }
+
+    // Print statistics
+    printf("Number of finished processes: %d\n", num_finished);
+    printf("Number of exceptions: %d\n", num_exceptions);
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        printf("Process %d: Running time = %d ms, Finish state = %d\n", i, running_time[i], finish_state[i]);
+    }
+
+//    return 0;
 }
 
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!\n");
+   int parent_pid = getpid();
+   printf("init %i\n", parent_pid);
    srand(time(NULL));
-   first(5);
+//   first(5);
+   printf("second %i vs new %i\n", parent_pid, getpid());
+   if(getpid() == parent_pid) {
+       second2(5);
+   }
    return 0;
 }
