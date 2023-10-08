1f7ebfb6f25d2b4099f3b2a0f75f9ca112c978df
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-10-08 22:21:11 +0200

    2.3 bkp

--- 2/proj/main.c
+++ 2/proj/main.c
@@ -5,14 +5,18 @@
 #include <stdlib.h>
 
 #include <sys/wait.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
 
 /**
  * Zadani:
  * 2.1. Vyzkoušejte, kolik procesů vám systém dovolí vytvořit procesů. { while fork…. while waitpid….
- * 2.2. Pro testování si raději nastavte ulimit -u 10000.
- * 2.3. Kolik současně běžících vláken můžete vytvořit. Kód vlákna bude { while ( 1 ) sleep( 1 ); return nullptr; }
- * 2.4. Kolik vláken můžete postupně vytvořit. Kód vlákna bude {return nullptr;} a nedělá se ani join ani detach.
- * 2.5. Udržujte si cca 1000 potomků, zachytávejte SIGCHLD, v sighandleru provádějte while ...waitpid... a počítejte počet signálů a počet ukončených potomků.
+ *      Pro testování si raději nastavte ulimit -u 10000.
+ *      Kolik současně běžících vláken můžete vytvořit. Kód vlákna bude { while ( 1 ) sleep( 1 ); return nullptr; }
+ * 2.2 Kolik vláken můžete postupně vytvořit. Kód vlákna bude {return nullptr;} a nedělá se ani join ani detach.
+ * 2.3. Udržujte si cca 1000 potomků, zachytávejte SIGCHLD, v sighandleru provádějte while ...waitpid... a počítejte počet signálů a počet ukončených potomků.
  *
  * 2.1.:
  *
@@ -30,10 +34,52 @@
  * Pro `ulimit -u 10000` a naslednem spusteni ./run.sh mi tento program
  * vygeneroval `8132` zaznamu o novych vlaknech.
  * Start 8132 : 67817 75950 67817 started
+ *
+ * Napadji me duvody proc 1868(=10000-8132) procesu nevyuzitych:
+ * a) main je samotny proces
+ * b) run.sh zabere nektere z mozneho poctu procesu
+ * c) samotny kompilator v run.sh muze bezet napric vice procesy
+ * d) TODO porovnat beh samotneho main
+ *
+ * 2.2 Kolik vláken můžete postupně vytvořit. Kód vlákna bude {return nullptr;} a nedělá se ani join ani detach.
+ * 10 vlaken viz soubor run.sh.2_2.log
+ * Duvod me napada, ze je:
+ * a) spatne zaznamenani bezicich potomku
+ * b)
+ *
+ * 2.3.
+ * TODO viz https://docs.oracle.com/cd/E19455-01/806-4750/signals-7/index.html
  */
- 
-int* second_assignment(int NUM_PROCESSES) {
 
+
+void proc_exit()
+{
+/*    int wstat;
+    union wait wstat;
+    pid_t	pid;
+
+    while (1) {
+        pid = wait3 (&wstat, WNOHANG, (struct rusage *)NULL );
+        if (pid == 0)
+            return;
+        else if (pid == -1)
+            return;
+        else
+            printf ("Return code: %d\n", wstat.w_retcode);
+    }*/
+    int wstat;
+    pid_t pid;
+
+    while (1) {
+        pid = wait3(&wstat, WNOHANG, NULL);
+        if (pid == 0 || pid == -1)
+            return;
+        printf("-- Return code: %d, %i\n", wstat, getpid());
+    }
+}
+
+int* second_assignment(int NUM_PROCESSES) {
+    signal (SIGCHLD, proc_exit);
     int running_time[NUM_PROCESSES];
     int finish_state[NUM_PROCESSES];
     pid_t pids[NUM_PROCESSES];
@@ -63,7 +109,8 @@ int* second_assignment(int NUM_PROCESSES) {
     }
 
     if (pid == 0) {
-        while ( 1 ) sleep( 1 ); return NULL;
+//        while ( 1 ) sleep( 1 ); return NULL;
+        return NULL;
     }
 
     if(getpid() == parent_pid) {
@@ -94,7 +141,13 @@ int* second_assignment(int NUM_PROCESSES) {
 //                    printf("?? - status of %i: %i s: %i, pid: %i run_time: %i\n", i, pids[i], finish_state[i], getpid(), running_time[i]);
                 }
             }
-
+        int status3 = -1;
+        pid_t result = waitpid(-1, &status3, WNOHANG);
+        if(result != -1) {
+            printf("\n?? No children processes broke the loop: statistics %i: pid: %i, %i, %i\n",
+                   num_finished, pid, getpid(), getppid());
+            break;
+        }
         } while (num_finished < NUM_PROCESSES);
 
         time_t cur_running_time = (time(NULL) - start_time_all);
@@ -117,6 +170,7 @@ int* second_assignment(int NUM_PROCESSES) {
 int main() {
    int parent_pid = getpid();
    srand(time(NULL));
-   second_assignment(10000);
+//    second_assignment(10000);
+    second_assignment(10);
    return 0;
 }
