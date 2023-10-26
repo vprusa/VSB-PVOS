2db6831768dfe2f3dabf87549b16c8f83016dcb4
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-10-26 12:36:26 +0200

    4. sketch

--- 4/proj/main.c
+++ 4/proj/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
-#include <memory.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/select.h>
+
+
 
 /**
  * Zadani:
@@ -17,12 +24,46 @@
 int readline( int fd, void *ptr, int len, int tout_ms );
 
 int main(int argc, char *argv[]) {
-    while(1) {
-        readline();
+    int fd;
+    char buffer[2048];
+    int result;
+    // TODO stty -F /dev/tty -icanon
+    // pro testovaci ucely testovaci soubor, pozdeji nutne resit cekani
+    fd = open("test.txt", O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return 1;
     }
+
+    result = readline(fd, buffer, sizeof(buffer) - 1, 100);
+
+    if (result == -1) {
+        printf("Chyba pri cteni\n");
+        // TODO rozlisit
+        //        Jen readline vrací timeout jako errno s hodnotou ETIME nebo ETIMEDOUT.
+        // TODO print do perror("xxx");
+    } else if (result == 0) {
+        printf("Prazdny vstup\n");
+    } else {
+        printf("Vystup: %s\n", buffer);
+    }
+
+    close(fd);
     return 0;
 }
 
+/**
+ * Precte z fd do ptr (buffer) len znaku, nebo do timeoutu
+ *
+ * @param fd
+ * @param ptr
+ * @param len
+ * @param tout_ms
+ * @return
+ */
 int readline( int fd, void *ptr, int len, int tout_ms) {
-    return 0;
+    int read_bytes = 0;
+
+
+    return read_bytes;
 }
