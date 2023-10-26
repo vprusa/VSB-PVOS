78e5bc3a233eaef40b4beca02c0e5054726fb582
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-10-26 20:27:40 +0200

    4. test

--- 4/proj/main.c
+++ 4/proj/main.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <sys/types.h>
 #include <sys/time.h>
 #include <sys/select.h>
-
-#include <errno.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-
 /**
  * Zadani:
  * 0. Pro čtení z terminálu po znacích si nastavte “stty -F /dev/tty -icanon”.
@@ -27,7 +16,7 @@
  * Viz. man gettimeofday a man timeradd & spol.
  */
 
-int readline( int fd, void *ptr, int len, int tout_ms );
+int readline( int fd, void *ptr, int len, int tout_ms);
 
 int main(int argc, char *argv[]) {
     int fd;
@@ -51,7 +40,7 @@ int main(int argc, char *argv[]) {
     } else if (result == 0) {
         printf("Prazdny vstup\n");
     } else {
-        printf("Vystup: %s\n", buffer);
+        printf("Vystup:\n%s\n", buffer);
     }
 
     close(fd);
@@ -67,46 +56,84 @@ int main(int argc, char *argv[]) {
  * @param tout_ms
  * @return
  */
-int readline( int fd, void *ptr, int buffer_size, int tout_ms) {
-    int read_bytes = 0;
-    char *buffer = ptr;
+int readline( int fd, void *ptr, int len, int tout_ms) {
+    int retval;
+    int bytes_read = 0;
+    int n;
 
-    int ix = 0, bytes_malloced = 0;
+    struct timeval timeout;
+    struct timeval now;
+    struct timeval endtime;
 
-    if (!buffer)
-    {
-        bytes_malloced = 64;
-        buffer = (char *)malloc(bytes_malloced);
-        buffer_size = bytes_malloced;
+    fd_set rfds;
+
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) return -1;
+    // nastaveni neblokujicich operaci nad souborem
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        return -1; // pokud se nepovede ukoncuji cteni
     }
 
-    for (;;++ix)
-    {
-        int ch;
+    // nastaveni casoveho limitu
+    gettimeofday(&now, NULL);
+    timeout.tv_sec = tout_ms / 1000;
+    timeout.tv_usec = (tout_ms % 1000) * 1000;
+    timeradd(&now, &timeout, &endtime);
 
-        if (ix == buffer_size - 1)
-        {
-            if (!bytes_malloced)
+    // smycka ve ktere ctu davky znaku ze souboru a kontroluji, ze nevypresel casovy limit
+    while (bytes_read < len) {
+        // nastavim deskriptor souboru do mnoziny deskriptoru, ze kterych se cte
+        FD_ZERO(&rfds);
+        FD_SET(fd, &rfds);
+
+        // aktualizuji casovy limit
+        gettimeofday(&now, NULL);
+        timersub(&endtime, &now, &timeout);
+
+        if (timeout.tv_sec < 0 || (timeout.tv_sec == 0 && timeout.tv_usec <= 0)) {
+            errno = ETIMEDOUT;
+            // pokud vyprsel timout neuspesne ukoncuji cteni
+            fcntl(fd, F_SETFL, flags);
+            return -1;
+        }
+
+        // vyberu deskriptor s maximalni dobu cekani na nej
+        retval = select(fd + 1, &rfds, NULL, NULL, &timeout);
+
+        // nepovedlo se ziskat deskriptor souboru
+        if (retval == -1) {
+            fcntl(fd, F_SETFL, flags);
+            return -1; // neuspesne ukoncuji cteni
+        } else if (retval) {
+            // ctu do bufferu s ofsetem jiz prectenych bytu zbytek nenacetenych bytu
+            n = read(fd, (char *)ptr + bytes_read, len - bytes_read);
+            if (n == -1) {
+                // cteni se nepovedlo
+                if (errno != EWOULDBLOCK && errno != EAGAIN) {
+                    // pokud neni chyba blokaci nebo moznosti cist znova, tak ukoncuji neuspesne cteni
+                    fcntl(fd, F_SETFL, flags);
+                    return -1;
+                }
+            } else if (n == 0) {
+                // uspesne cteni, ale konec souboru
                 break;
-            bytes_malloced += bytes_malloced;
-            buffer = (char *) realloc(buffer, bytes_malloced);
-            buffer_size = bytes_malloced;
+            } else {
+                // zvendu hodnotu citace prectenych znaku hodnotu naposledy prectenych znaku
+                bytes_read += n;
+            }
+        } else {
+            // cas vyprsel, neuspesne ukoncuji
+            errno = ETIMEDOUT;
+            fcntl(fd, F_SETFL, flags);
+            return -1;
         }
-
-        ch = getchar();
-        if (ch == EOF)
-        {
-            if (bytes_malloced)
-                free(buffer);
-            return 0;
-        }
-        if (ch == '\n')
-            break;
-        buffer[ix] = ch;
     }
 
-    /* 0 terminate buffer. */
-    buffer[ix] = '\0';
+    // vratim rezim souboru do puvodniho stavu
+    if (fcntl(fd, F_SETFL, flags) == -1) {
+        // v pripade, ze se nepovedlo nastavit rezim souboru do puvodniho stavu, tak neuspesne ukoncuji cteni
+        return -1;
+    }
 
-    return read_bytes;
+    return bytes_read;
 }
