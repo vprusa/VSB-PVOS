8f10e66f741670f55ced9ac40acdbf6e33936d0e
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-11-06 17:31:34 +0100

    5. init, bkp

--- 5/proj/main.c
+++ 5/proj/main.c
@@ -8,24 +8,34 @@
 #include <sys/mman.h>
 #include <sys/stat.h>        /* For mode constants */
 #include <fcntl.h>           /* For O_* constants */
-// sender.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/select.h>
 
 /**
  * Zadani:
  *    Upravte příklad pro SIGIO a rouru na příklad s N potomky a N rourami.
+ *    - wtf ... ktery priklad? ...
  *    Sighandler bude využívat strukturu siginfo_t.
+ *    - wtf #2
  *    Upravte příklad pro aio tak, aby místo SIGEV_NONE využil SIGEV_SIGNAL a SIGEV_THREAD.
  *    Opět N potomků a N rour.
+ *    - wtf #3
+ *
  *    Naimplementujte si funkci readline_tout s vyrovnávacím bufferem. Maximálně se snažte,
  *    aby se funkce readline_tout chovala podobně, jako read.
  *        readline je programátorská etuda - implementace řady problémů
  *        může být vyrovnávací buffer lokální proměnná ve funkci/metodě?
  *        - záleží na implementaci, v případě užití singálů, které používají metody pro zpracování signálu bude použití
- *        -- jedné lokální proměnné zamezeno přístupem
+ *        -- jedné lokální proměnné zamezeno přístupem ke sdíleným datům
  *        může být pouze jeden buffer? Jak spárovat buffer a file descriptor?
  *        -- budeme-li používat globální promennou napříč voláním `main` i funkce `sighandleru`, které sdílí svá datat,
  *        -- tak není nutno pouřívat file
@@ -34,7 +44,7 @@
  *        co když je řádek delší než buffer?'
  *        - pak buffer načte své maximum a zbytek zahodí, v návratové kódu bude indikace BUFFER overflow,
  *        --- program může mít vstupní parametr vlaječky určující striktost vůči přetečení bufferu a způsob vyřízení
- *        ---této situace
+ *        --- této situace
  *        může se přepsat na konci řádku?
  *        - pro náš případ ne a nejsem si vědom, že bych se s touto situací prakticky setkal.
  *        -- teoreticky je možné kdeco. např. přepsat buffer na konci řádku bude-li nastaveno parametrem vlaječky atp.
@@ -52,6 +62,8 @@
 #define SHM_NAME_PID "myshmpid"
 #define ARR_SIZE 10
 
+#define BUFFER_SIZE 2048
+
 #define DEBUG
 
 #ifdef DEBUG
@@ -60,203 +72,195 @@
 #else
 #define debug(fmt, ...) // Nothing
 #endif
-int app_1();
-int app_2();
 
-/**
- * https://www.geeksforgeeks.org/posix-shared-memory-api/
- * @return
- */
-int app_1() {
-    debug("app_1 - start");
+static volatile char buffer[BUFFER_SIZE];
 
+int readline( int fd, void *ptr, int len, int tout_ms);
 
-    /* get pid of app_2 and send notification to it */
+int main(int argc, char *argv[]) {
+//    int fd;
+    int timelimit = 5000;
+//    int mknod(const char * pathname , mode_t mode, dev_t dev);
+//    int mkfifo(const char * pathname , mode_t mode);
+//    int result =  mkfifo("mypipe", O_RDWR);
 
-    /* get pid of app_2 */
-    int segment_id_pid = shm_open(SHM_NAME_PID, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    int size_pid = sizeof(int);
-    ftruncate(segment_id_pid, 1);
-    int* ptr_pid;
-    /* memory map the shared memory object */
-    ptr_pid = mmap(0, size_pid, PROT_WRITE, MAP_SHARED, segment_id_pid, 0);
-    ptr_pid[0] = -1; //getpid();
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
 
-    pid_t apt2_pid = -1;
-    while(1) {
-        debug("app_1 - waiting for pid to appear ");
-        sleep(1);
-        int* ptr_pid2; // pointer to shared memory object
-        int shm_fd_pid2 = shm_open(SHM_NAME_PID, O_RDONLY, 0666); // shared memory file descriptor
-
-        /* memory map the shared memory object */
-        ptr_pid2 = mmap(0, size_pid, PROT_READ, MAP_SHARED, shm_fd_pid2, 0);
-        apt2_pid = ptr_pid2[0];
-        if(apt2_pid != -1) {
-            break;
-        }
-    }
-    shm_unlink(SHM_NAME_PID);
-
-    debug("app_1 - app_2 pid is %d, going to write to shared memory....", apt2_pid);
-    // in apt2_pid there is pid of second app
-    // now lets write to shared memory list of numbers and its sum
-
-    int segment_id = shm_open(SHM_NAME, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    int size = sizeof(int) * ARR_SIZE + 1;
-    ftruncate(segment_id, size);
-    int* ptr;
-    /* memory map the shared memory object */
-    ptr = mmap(0, size, PROT_WRITE, MAP_SHARED, segment_id, 0);
-
-    /* write to the shared memory object */
-    for (int i = 0; i < ARR_SIZE; i++) {
-        ptr[i] = i;
-        ptr[ARR_SIZE] += i;
+    // Continuously read from stdin and print the data to stdout
+    while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
+        write(STDOUT_FILENO, buffer, bytes_read);
     }
 
-    debug("app_1 - sum is %d, sending notify to app_2", ptr[ARR_SIZE]);
-
-    // now lets notify the other process that we wrote to shared memory
-    if (kill(apt2_pid, SIGUSR1) == -1) {
-        perror("kill");
+    if (bytes_read == -1) {
+        perror("read");
         exit(EXIT_FAILURE);
-    }
-
-    sleep(2);
-    shm_unlink(SHM_NAME);
-    debug("app_2 - end");
-    // TODO readline_tout
-    return 0;
-}
-
-void signal_handler(int signum) {
-    printf("app_2 - Received signal %d\n", signum);
-
-    sleep(1);
-    int size = sizeof(int) * ARR_SIZE + 1;
-
-    int* ptr; // pointer to shared memory object
-    int shm_fd = shm_open(SHM_NAME, O_RDONLY, 0666); // shared memory file descriptor
-    debug("app_2 - mmap");
-
-    /* memory map the shared memory object */
-    ptr = mmap(0, size, PROT_READ, MAP_SHARED, shm_fd, 0);
-
-    debug("app_2 - sum");
-    /* read from the shared memory object */
-    int sum = ptr[ARR_SIZE];
-    debug("app_2 - sum2");
-    printf("app_2 - sum: %d\n", sum);
-    debug("app_2 - sum_check - start");
-
-    int sum_check = 0;
-    /* read and sum check */
-    for (int i = 0; i < ARR_SIZE; i++) {
-        sum_check += ptr[i];
-    }
-    debug("app_2 - sum_check_done - start");
-    printf("app_2 - sum_check: %d", sum_check);
-    if(sum == sum_check) {
-        printf(" - OK");
-    }else{
-        printf(" - KO");
-    }
-    printf("\n");
-    // TODO readline_tout
-    exit(EXIT_SUCCESS);
-}
-
-int app_2() {
-    debug("app_2 - start");
-
-    printf("app_2 - Receiver PID: %d\n", getpid());
-
-    // Register the signal handler
-    struct sigaction sa;
-    sa.sa_handler = signal_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-    debug("app_2 - write current pid %d to shared memory for pid", getpid());
-
-//     write pid to shared memory (and wait till it is read)
-//    while(1) {
-    int segment_id_pid = shm_open(SHM_NAME_PID, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    int size_pid = sizeof(int);
-    ftruncate(segment_id_pid, size_pid);
-    int *ptr_pid;
-    /* memory map the shared memory object */
-    ptr_pid = mmap(0, size_pid, PROT_WRITE, MAP_SHARED, segment_id_pid, 0);
-
-    /* write current pid to the shared memory object */
-    ptr_pid[0] = getpid();
-//        sleep(1);
-//        break;
-//    }
-    debug("app_2 - pause");
-    /* read from the shared memory object */
-    // Wait for the signal indefinitely
-    while (1) {
-        pause();
     }
 
 /*
-    sleep(1);
-    int size = sizeof(int) * ARR_SIZE + 1;
+    int fd;
+//    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
 
+    // Open the FIFO for reading
+//    fd = open("/tmp/myfifo", O_RDONLY);
+//    if (fd == -1) {
+//        perror("open");
+//        exit(EXIT_FAILURE);
+//    }
 
-    int shm_fd; // shared memory file descriptor
-    int* ptr; // pointer to shared memory object
-    shm_fd = shm_open(SHM_NAME, O_RDONLY, 0666);
-    debug("app_2 - mmap");
-
-    *//* memory map the shared memory object *//*
-    ptr = mmap(0, size, PROT_READ, MAP_SHARED, shm_fd, 0);
-
-    debug("app_2 - sum");
-    *//* read from the shared memory object *//*
-    int sum = ptr[ARR_SIZE];
-    debug("app_2 - sum2");
-    printf("app_2 - sum: %d\n", sum);
-    debug("app_2 - sum_check - start");
-
-    int sum_check = 0;
-    *//* read and sum check *//*
-    for (int i = 0; i < ARR_SIZE; i++) {
-        sum_check += ptr[i];
+    // Continuously read from FIFO and print the data to standard output
+    while (1) {
+//        bytes_read = read(fd, buffer, BUFFER_SIZE);
+        if (bytes_read > 0) {
+//            write(STDOUT_FILENO, buffer, bytes_read);
+            printf("res (%d): %s\n", bytes_read, buffer);
+        } else if (bytes_read == 0) {
+            // No more data, the writing side of the FIFO was closed
+            break;
+        } else {
+            // An error occurred
+            perror("read");
+//            close(fd);
+//            exit(EXIT_FAILURE);
+        }
     }
-    debug("app_2 - sum_check_done - start");
-    printf("app_2 - sum_check: %d", sum_check);
-    if(sum == sum_check) {
-        printf(" - OK");
-    }else{
-        printf(" - KO");
-    }
-    printf("\n");*/
 
-    debug("app_2 - unlink");
-    shm_unlink(SHM_NAME);
-    debug("app_2 - end");
+    close(fd);
+*/
+    // pro testovaci ucely testovaci soubor, pozdeji nutne resit cekani
+    // fd = open("test.txt", O_RDONLY);
+    // dalsi moznost je pouzit tty dle: `stty -F /dev/tty -icanon`
+    /*fd = open("/dev/tty", O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    int result = readline(fd, buffer, sizeof(buffer) - 1, timelimit);
+
+    // vyhodnoceni vystupu
+    if (result == -1) {
+        if (errno == ETIMEDOUT) {
+            printf("\nreadline - chyba pri cteni - dosazen timelimit: %d\n", timelimit);
+            perror("readline - timeout\n");
+//            perror("readline - chyba pri cteni - dosazen timelimit: %d\n", timelimit);
+        } else {
+            perror("readline - chyba pri cteni\n");
+        }
+    } else if (result == 0) {
+        perror("prazdny vstup\n");
+    } else {
+        buffer[result] = '\0';  // ukončení řetězce
+        printf("Vystup: %s\n", buffer);
+    }
+
+    close(fd);*/
     return 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s [1|2]\n", argv[0]);
-        return 2;
-    } else {
-        switch (argv[1][0]) {
-            case '1':
-                return app_1();
-            case '2':
-                return app_2();
-            default:
-                printf("Usage: %s [1|2]\n", argv[0]);
+/**
+ * Precte z fd do ptr (buffer) len znaku, nebo do timeoutu
+ *
+ * @param fd
+ * @param ptr
+ * @param len
+ * @param tout_ms
+ * @return
+ */
+int readline( int fd, void *ptr, int len, int tout_ms) {
+    // inicializace promennych
+    int retval;
+    int bytes_read = 0; // pocet prectenych bytu
+    int n; // citac nasledy prectenych bytu
+
+    // pomocne promenne pro rizeni casovani
+    struct timeval timeout;
+    struct timeval now;
+    struct timeval endtime;
+
+    // promenna pro mnozinu deskriptoru ze kterych se cte
+    fd_set rfds;
+
+    // nastaveni flajecek dekstiprotu
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) {
+        // nepovedlo se nastavit vlajecky deksriptoru a tak neuspesne ukoncuji cteni
+        return -1;
+    }
+    // nastaveni neblokujicich operaci nad souborem
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        return -1; // pokud se nepovede ukoncuji cteni
+    }
+
+    // nastaveni casoveho limitu
+    gettimeofday(&now, NULL);
+    timeout.tv_sec = tout_ms / 1000;
+    timeout.tv_usec = (tout_ms % 1000) * 1000;
+    timeradd(&now, &timeout, &endtime);
+
+    // smycka ve ktere ctu davky znaku ze souboru a kontroluji, ze nevypresel casovy limit
+    while (bytes_read < len) {
+        // nastavim deskriptor souboru do mnoziny deskriptoru, ze kterych se cte
+        FD_ZERO(&rfds);
+        FD_SET(fd, &rfds);
+
+
+        // aktualizuji cas
+        gettimeofday(&now, NULL);
+        timersub(&endtime, &now, &timeout);
+
+        // pokud vyprsel casovy limit nebo doslo k chybe, tak ukoncuji cteni
+        if (timeout.tv_sec < 0 || (timeout.tv_sec == 0 && timeout.tv_usec <= 0)) {
+            errno = ETIMEDOUT;
+            // pokud vyprsel timout neuspesne ukoncuji cteni
+            fcntl(fd, F_SETFL, flags);
+            return -1;
+        }
+
+        // vyberu deskriptor s maximalni dobu cekani na nej
+        retval = select(fd + 1, &rfds, NULL, NULL, &timeout);
+
+        // nepovedlo se ziskat deskriptor souboru
+        if (retval == -1) {
+            fcntl(fd, F_SETFL, flags);
+            return -1; // neuspesne ukoncuji cteni
+        } else if (retval) {
+            // ctu do bufferu s ofsetem jiz prectenych bytu zbytek nenacetenych bytu
+            n = read(fd, (char *)ptr + bytes_read, len - bytes_read);
+            if (n == -1) {
+                // cteni se nepovedlo
+                if (errno != EWOULDBLOCK && errno != EAGAIN) {
+                    // pokud neni chyba blokaci nebo moznosti cist znova, tak ukoncuji neuspesne cteni
+                    fcntl(fd, F_SETFL, flags);
+                    return -1;
+                }
+            } else if (
+                    n == 0 ||
+                    ((char *)ptr + bytes_read)[0] == '\n' ||
+                    ((char *)ptr + bytes_read)[0] == '\r'
+                    ) {
+                // uspesne cteni, ale konec souboru
+                char * last_char = ((char *)(ptr + bytes_read + n));
+                last_char[0]= '\0';
+                break;
+            } else {
+                // zvendu hodnotu citace prectenych znaku hodnotu naposledy prectenych znaku
+                bytes_read += n;
+            }
+        } else {
+            // cas vyprsel, neuspesne ukoncuji
+            errno = ETIMEDOUT;
+            fcntl(fd, F_SETFL, flags);
+            return -1;
         }
     }
-    return 2;
+
+    // vratim rezim souboru do puvodniho stavu
+    if (fcntl(fd, F_SETFL, flags) == -1) {
+        // v pripade, ze se nepovedlo nastavit rezim souboru do puvodniho stavu, tak neuspesne ukoncuji cteni
+        return -1;
+    }
+
+    return bytes_read;
 }
