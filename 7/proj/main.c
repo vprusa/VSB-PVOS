d4a5625d6e19a9c42befd968b2f8b88d90164887
Vojtěch Průša <prusa.vojtech@gmail.com>
2023-11-18 22:08:16 +0100

    7. bkp

--- 7/proj/main.c
+++ 7/proj/main.c
@@ -14,6 +14,15 @@
 #include <signal.h>
 #include <unistd.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>  // Include POSIX semaphore library
+#include <sys/sem.h>
+
+
 /**
  * Zadani:
  * Pomocí semaforu System V i Posix implementujte problém “plnění přepravky”. 
@@ -24,17 +33,14 @@
  *    2. Druhý (program) proces bude přepravky vyměňovat. Po výměně rozběhne “plniče”.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
 
 // ANSI escape codes for color formatting
 #define GREEN_COLOR "\033[0;32m"
 #define ORANGE_COLOR "\033[0;33m"
 #define BLUE_COLOR "\033[0;34m"
+#define MAGNETA_COLOR "\033[0;35m"
+#define CYAN_COLOR "\033[0;36m"
+#define BRIGHT_CYAN_COLOR "\033[0;96m"
 #define RESET_COLOR "\033[0m"
 
 #define DEBUG
@@ -55,6 +61,8 @@
 #define SHM_KEY 1234
 #define SEM_KEY 5678
 
+#define X_SEM_NAME "/x_sem"  // Unique name for the POSIX semaphore
+
 // Structure for the crate
 struct Crate {
     int capacity;
@@ -66,33 +74,11 @@ struct Crate {
 struct sembuf wait_op = {0, -1, 0};
 struct sembuf signal_op = {0, 1, 0};
 
-int app_common();
 int app_1(int app_1_cnt);
 int app_2();
 
-int app_common_X();
 int app_1_X(int app_1_cnt);
 int app_2_X();
-
-// Deklarace funkcí pro manipulaci se semafory
-int sem_wait(int sem_id);
-int sem_signal(int sem_id);
-
-int app_common() {
-    return 0;
-}
-
-int app_common_X() {
-    return -1;
-}
-
-int app_1_X(int app_1_cnt) {
-    return -1;
-}
-
-int app_2_X() {
-    return -1;
-}
 
 /*
  *    1. Jeden (program) proces bude plnit přepravku - sdílenou paměť - výrobky.
@@ -123,29 +109,29 @@ int app_1(int app_1_cnt) {
     crate->sem_id = 0;
 
     while (app_1_cnt > 0) {
-        debug(GREEN_COLOR "   app_1: " RESET_COLOR "Waiting for permission to proceed");
+        debug(MAGNETA_COLOR "   app_1: " RESET_COLOR "Waiting for permission to proceed");
 
         int counter = APP_1_MAX_COUNTER; // Initialize the counter to 1000
         while (counter > 0) {
             // Debug message with green color prefix
-            debug(GREEN_COLOR "   app_1: " RESET_COLOR "Waiting for permission to proceed");
+            debug(MAGNETA_COLOR "   app_1: " RESET_COLOR "Waiting for permission to proceed");
             semop(crate->sem_id, &wait_op, 1); // Wait for permission to proceed
-            debug(GREEN_COLOR "   app_1: " RESET_COLOR "Proceeding...");
+            debug(MAGNETA_COLOR "   app_1: " RESET_COLOR "Proceeding...");
 
             if (crate->current_load < crate->capacity) {
                 crate->current_load++;
-                debug(GREEN_COLOR "app_1: " RESET_COLOR "Filling the crate, current load: %d/%d",
+                debug(MAGNETA_COLOR "app_1: " RESET_COLOR "Filling the crate, current load: %d/%d",
                       crate->current_load, crate->capacity);
             } else {
-                debug("  " GREEN_COLOR "app_1: " RESET_COLOR "Exchanging the crate");
+                debug("  " MAGNETA_COLOR "app_1: " RESET_COLOR "Exchanging the crate");
     //            crate->current_load = 0;
             }
 
-            debug(GREEN_COLOR "   app_1: " RESET_COLOR "Signaling completion of the task start");
+            debug(MAGNETA_COLOR "   app_1: " RESET_COLOR "Signaling completion of the task start");
             semop(crate->sem_id, &signal_op, 1); // Signal completion of the task
 
             // Debug message with green color prefix
-            printf(GREEN_COLOR "   app_1: " RESET_COLOR "Signaling completion of the task end");
+            debug(MAGNETA_COLOR "   app_1: " RESET_COLOR "Signaling completion of the task end");
 
             counter--; // Decrement the counter
             //        sleep(1);
@@ -192,7 +178,7 @@ int app_2() {
         } else {
             debug(ORANGE_COLOR "  app_2: " RESET_COLOR "Empty crate" RESET_COLOR);
 //            crate->current_load = 0;
-            debug(GREEN_COLOR "   app_2: " RESET_COLOR "Signaling completion of the task start");
+            debug(ORANGE_COLOR "   app_2: " RESET_COLOR "Signaling completion of the task start");
             semop(crate->sem_id, &signal_op, 1); // Signal completion of the task
         }
 
@@ -202,6 +188,159 @@ int app_2() {
     }
 
     shmdt(crate);
+
+    return 0;
+}
+
+/*
+ *    1. Jeden (program) proces bude plnit přepravku - sdílenou paměť - výrobky.
+ *       Do přepravky může v dané chvíli přistupovat jen jeden proces.
+ *       Přepravka má kapacitu a aktuální stav.
+ */
+int app_1_X(int app_1_cnt) {
+    // Attempt to find the existing shared memory segment
+    int shm_id = shmget(SHM_KEY, sizeof(struct Crate), 0);
+
+    if (shm_id == -1) {
+        // If the shared memory segment doesn't exist, create it
+        shm_id = shmget(SHM_KEY, sizeof(struct Crate), IPC_CREAT | 0666);
+        if (shm_id == -1) {
+            perror("shmget");
+            return 1;
+        }
+    }
+
+    struct Crate *crate = (struct Crate *)shmat(shm_id, NULL, 0);
+    if (crate == (struct Crate *)(-1)) {
+        perror("shmat");
+        return 1;
+    }
+
+    crate->current_load = 0;
+    crate->capacity = MAX_CAPACITY;
+
+    // Create a POSIX semaphore
+    sem_t *sem = sem_open(X_SEM_NAME, O_CREAT, 0666, 1);  // Initial value 1 for mutual exclusion
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        return 1;
+    }
+
+    while (app_1_cnt > 0) {
+        debug(BLUE_COLOR "   app_1_X: " RESET_COLOR "Waiting for permission to proceed");
+
+        int counter = APP_1_MAX_COUNTER; // Initialize the counter
+        while (counter > 0) {
+            // Debug message with green color prefix
+            debug(BLUE_COLOR "   app_1_X: " RESET_COLOR "Waiting for permission to proceed");
+
+            // Wait for permission to proceed using POSIX semaphore
+            if (sem_wait(sem) == -1) {
+                perror("sem_wait");
+                return 1;
+            }
+            debug(BLUE_COLOR "   app_1_X: " RESET_COLOR "Proceeding...");
+
+            if (crate->current_load < crate->capacity) {
+                crate->current_load++;
+                debug(BLUE_COLOR "app_1_X: " RESET_COLOR "Filling the crate, current load: %d/%d",
+                      crate->current_load, crate->capacity);
+            } else {
+                debug("  " BLUE_COLOR "app_1_X: " RESET_COLOR "Exchanging the crate");
+            }
+
+            // Signal completion of the task using POSIX semaphore
+            if (sem_post(sem) == -1) {
+                perror("sem_post");
+                return 1;
+            }
+
+            counter--; // Decrement the counter
+            usleep(100000);
+        }
+        sleep(5);
+        app_1_cnt--;
+    }
+
+    shmdt(crate);
+
+    // Close and unlink the semaphore
+    if (sem_close(sem) == -1) {
+        perror("sem_close");
+        return 1;
+    }
+    if (sem_unlink(X_SEM_NAME) == -1) {
+        perror("sem_unlink");
+        return 1;
+    }
+
+    return 0;
+}
+
+/*
+ *    2. Druhý (program) proces bude přepravky vyměňovat. Po výměně rozběhne “plniče”.
+ */
+int app_2_X() {
+    // sanity wait
+    sleep(1);
+
+    // Attaching to the existing shared memory
+    int shm_id = shmget(SHM_KEY, sizeof(struct Crate), 0666);
+    if (shm_id == -1) {
+        perror("shmget");
+        return 1;
+    }
+
+    struct Crate *crate = (struct Crate *)shmat(shm_id, NULL, 0);
+    if (crate == (struct Crate *)(-1)) {
+        perror("shmat");
+        return 1;
+    }
+
+    // Create a POSIX semaphore
+    sem_t *sem = sem_open(X_SEM_NAME, 0);  // Open the existing semaphore
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        return 1;
+    }
+
+    int counter = APP_2_MAX_COUNTER; // Initialize the counter
+    while (counter > 0) {
+        debug(BRIGHT_CYAN_COLOR "  app_2_X: " RESET_COLOR "Waiting for permission to proceed");
+
+        // Wait for permission to proceed using POSIX semaphore
+        if (sem_wait(sem) == -1) {
+            perror("sem_wait");
+            return 1;
+        }
+        debug(BRIGHT_CYAN_COLOR "  app_2_X: " RESET_COLOR "Proceeding...");
+
+        if (crate->current_load > 0) {
+            crate->current_load--;
+            debug(BRIGHT_CYAN_COLOR "app_2_X: " RESET_COLOR "Emptying crate, current load: %d/%d" RESET_COLOR,
+                  crate->current_load, crate->capacity);
+        } else {
+            debug(BRIGHT_CYAN_COLOR "  app_2_X: " RESET_COLOR "Empty crate" RESET_COLOR);
+        }
+
+        // Signal completion of the task using POSIX semaphore
+        if (sem_post(sem) == -1) {
+            perror("sem_post");
+            return 1;
+        }
+
+        debug(BRIGHT_CYAN_COLOR "  app_2_X: " RESET_COLOR "Signaling completion of the task");
+        counter--; // Decrement the counter
+        sleep(1);
+    }
+
+    shmdt(crate);
+
+    // Close the semaphore (unlinking is not required for existing semaphore)
+    if (sem_close(sem) == -1) {
+        perror("sem_close");
+        return 1;
+    }
 
     return 0;
 }
@@ -224,7 +363,7 @@ int main(int argc, char *argv[]) {
     } else {
         char c = 'V';
         if (argc > 2) {
-            c = argv[1][1];
+            c = argv[2][0];
         }
         switch (c) {
             // Using posix
--- 7/proj/run.sh
+++ 7/proj/run.sh
@@ -7,6 +7,24 @@ gcc "${MAIN_FILE}" -o "${OUT_FILE}"
 
 stty -F /dev/tty -icanon
 
-./${OUT_FILE} '1' & disown
-#./${OUT_FILE} '1'
-./${OUT_FILE} '2'
+#./${OUT_FILE} '1' & disown
+##./${OUT_FILE} '1'
+#./${OUT_FILE} '2'
+
+if [[ -n "${1}" && "${1}" = *"X"* ]]; then
+  ./${OUT_FILE} '1' 'X' '2' & disown
+  #./${OUT_FILE} '1'
+  ./${OUT_FILE} '2' 'X' '2'
+elif [[ -n "${1}" && "${1}" = *"V"* ]]; then
+  ./${OUT_FILE} '1' 'V' '2' & disown
+  #./${OUT_FILE} '1'
+  ./${OUT_FILE} '2' 'V' '2'
+else
+  echo "System V crate synchronization"
+  ./${OUT_FILE} '1' 'V' '2' & disown
+  ./${OUT_FILE} '2' 'V' '2'
+  sleep 5
+  echo "POSIX crate synchronization"
+  ./${OUT_FILE} '1' 'X' '2' & disown
+  ./${OUT_FILE} '2' 'X' '2'
+fi
