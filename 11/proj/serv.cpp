639033d29cc5c981b7ab6cab6e1e6ed279ce40d6
Vojtěch Průša <prusa.vojtech@gmail.com>
2024-01-24 22:27:19 +0100

    PVOS #11 multiple clients

--- 11/proj/serv.cpp
+++ 11/proj/serv.cpp
@@ -1,10 +1,8 @@
 /* serv.cpp  -  Minimal ssleay server for Unix
-   30.9.1996, Sampo Kellomaki <sampo@iki.fi> */
+   30.9.1996, Sampo Kellomaki <sampo@iki.fi>
 
-
-/* mangled to work with SSLeay-0.9.0b and OpenSSL 0.9.2b
-   Simplified to be even more minimal
-   12/98 - 4/99 Wade Scholine <wades@mail.cybg.com> */
+   Modified to accept multiple clients using fork()
+*/
 
 #include <stdio.h>
 #include <unistd.h>
@@ -16,6 +14,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <signal.h> /* For signal() */
 
 #include <openssl/rsa.h>       /* SSLeay stuff */
 #include <openssl/crypto.h>
@@ -24,129 +23,162 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-
-/* define HOME to be dir for key and cert files... */
 #define HOME "./"
-/* Make these what you want for cert & key files */
 #define CERTF  HOME "my.crt"
 #define KEYF  HOME  "my.key"
-
 
 #define CHK_NULL(x) if ((x)==NULL) exit (1)
 #define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
 #define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
 
-int main ()
-{
-  int err;
-  int listen_sd;
-  int sd;
-  struct sockaddr_in sa_serv;
-  struct sockaddr_in sa_cli;
-  socklen_t client_len;
-  SSL_CTX* ctx;
-  SSL*     ssl;
-  X509*    client_cert;
-  char*    str;
-  char     buf [4096];
-  const SSL_METHOD *meth;
-  
-  /* SSL preliminaries. We keep the certificate and key with the context. */
+void handle_client(int sd, SSL_CTX* ctx); // Function to handle client communication
 
-  SSL_load_error_strings();
-  SSLeay_add_ssl_algorithms();
-  meth = SSLv23_server_method();
-  ctx = SSL_CTX_new (meth);
-  if (!ctx) {
-    ERR_print_errors_fp(stderr);
-    exit(2);
-  }
-  
-  if (SSL_CTX_use_certificate_file(ctx, CERTF, SSL_FILETYPE_PEM) <= 0) {
-    ERR_print_errors_fp(stderr);
-    exit(3);
-  }
-  if (SSL_CTX_use_PrivateKey_file(ctx, KEYF, SSL_FILETYPE_PEM) <= 0) {
-    ERR_print_errors_fp(stderr);
-    exit(4);
-  }
+int main () {
+    // First lets setup listening for incoming connections
+    int listen_sd;
+    int client_fd;
+    struct sockaddr_in socketAddrClient;
+    socklen_t client_len;
 
-  if (!SSL_CTX_check_private_key(ctx)) {
-    fprintf(stderr,"Private key does not match the certificate public key\n");
-    exit(5);
-  }
+    int sd;
+    struct sockaddr_in sa_serv;
+    const SSL_METHOD *meth;
+    SSL_CTX* ctx;
 
-  /* ----------------------------------------------- */
-  /* Prepare TCP socket for receiving connections */
+    SSL_load_error_strings();
+    SSLeay_add_ssl_algorithms();
+    meth = SSLv23_server_method();
+    ctx = SSL_CTX_new (meth);
+    if (!ctx) {
+        ERR_print_errors_fp(stderr);
+        exit(2);
+    }
 
-  listen_sd = socket (AF_INET, SOCK_STREAM, 0);   CHK_ERR(listen_sd, "socket");
-  
-  memset (&sa_serv, '\0', sizeof(sa_serv));
-  sa_serv.sin_family      = AF_INET;
-  sa_serv.sin_addr.s_addr = INADDR_ANY;
-  sa_serv.sin_port        = htons (1111);          /* Server Port number */
-  
-  err = bind(listen_sd, (struct sockaddr*) &sa_serv,
-	     sizeof (sa_serv));                   CHK_ERR(err, "bind");
-	     
-  /* Receive a TCP connection. */
-	     
-  err = listen (listen_sd, 5);                    CHK_ERR(err, "listen");
-  
-  client_len = sizeof(sa_cli);
-  sd = accept (listen_sd, (struct sockaddr*) &sa_cli, &client_len);
-  CHK_ERR(sd, "accept");
-  close (listen_sd);
+    if (SSL_CTX_use_certificate_file(ctx, CERTF, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(3);
+    }
+    if (SSL_CTX_use_PrivateKey_file(ctx, KEYF, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(4);
+    }
 
-  printf ("Connection from %x, port %x\n",
-	  sa_cli.sin_addr.s_addr, sa_cli.sin_port);
-  
-  /* ----------------------------------------------- */
-  /* TCP connection is ready. Do server side SSL. */
+    if (!SSL_CTX_check_private_key(ctx)) {
+        fprintf(stderr,"Private key does not match the certificate public key\n");
+        exit(5);
+    }
 
-  ssl = SSL_new (ctx);                           CHK_NULL(ssl);
-  SSL_set_fd (ssl, sd);
-  err = SSL_accept (ssl);                        CHK_SSL(err);
-  
-  /* Get the cipher - opt */
-  
-  printf ("SSL connection using %s\n", SSL_get_cipher (ssl));
-  
-  /* Get client's certificate (note: beware of dynamic allocation) - opt */
+    listen_sd = socket (AF_INET, SOCK_STREAM, 0);   CHK_ERR(listen_sd, "socket");
 
-  client_cert = SSL_get_peer_certificate (ssl);
-  if (client_cert != NULL) {
-    printf ("Client certificate:\n");
-    
-    str = X509_NAME_oneline (X509_get_subject_name (client_cert), 0, 0);
-    CHK_NULL(str);
-    printf ("\t subject: %s\n", str);
-    OPENSSL_free (str);
-    
-    str = X509_NAME_oneline (X509_get_issuer_name  (client_cert), 0, 0);
-    CHK_NULL(str);
-    printf ("\t issuer: %s\n", str);
-    OPENSSL_free (str);
-    
-    /* We could do all sorts of certificate verification stuff here before
-       deallocating the certificate. */
-    
-    X509_free (client_cert);
-  } else
-    printf ("Client does not have certificate.\n");
+    int use_thread = 0;
+    int use_fork = 0;
+    if(use_fork) {
+        int fid = fork();
+        if (fid == 0) { // Child process
+            close(listen_sd); // Close listening socket in child
+            handle_client(sd, ctx); // Process the client's requests
+            exit(0);
+        } else if (fid > 0) { // Parent process
+            close(sd); // Close connected socket in parent
+        } else {
+            perror("Fork failed");
+            exit(1);
+        }
+    }
 
-  /* DATA EXCHANGE - Receive message and send reply. */
+    memset (&sa_serv, '\0', sizeof(sa_serv));
+    sa_serv.sin_family      = AF_INET;
+    sa_serv.sin_addr.s_addr = INADDR_ANY;
+    sa_serv.sin_port        = htons (1111);          /* Server Port number */
 
-  err = SSL_read (ssl, buf, sizeof(buf) - 1);                   CHK_SSL(err);
-  buf[err] = '\0';
-  printf ("Got %d chars:'%s'\n", err, buf);
-  
-  err = SSL_write (ssl, "I hear you.", strlen("I hear you."));  CHK_SSL(err);
+    int err = bind(listen_sd, (struct sockaddr*) &sa_serv, sizeof (sa_serv)); CHK_ERR(err, "bind");
+    err = listen (listen_sd, 5);                    CHK_ERR(err, "listen");
 
-  /* Clean up. */
+    /*
+    while (1) {
+        struct sockaddr_in sa_cli;
+        client_len = sizeof(sa_cli);
+        sd = accept (listen_sd, (struct sockaddr*) &sa_cli, &client_len);
+        CHK_ERR(sd, "accept");
+        printf ("Connection from %x, port %x\n", sa_cli.sin_addr.s_addr, sa_cli.sin_port);
 
-  close (sd);
-  SSL_free (ssl);
-  SSL_CTX_free (ctx);
+        int pid = fork();
+        if (pid == 0) { // Child process
+            close(listen_sd); // Close listening socket in child
+            handle_client(sd, ctx); // Process the client's requests
+            exit(0);
+        } else if (pid > 0) { // Parent process
+            close(sd); // Close connected socket in parent
+        } else {
+            perror("Fork failed");
+            exit(1);
+        }
+    }
+    */
+
+    while (1) {
+        client_len = sizeof(socketAddrClient);
+        client_fd = accept(listen_sd, (struct sockaddr*)&socketAddrClient, &client_len);
+        CHK_ERR(client_fd, "accept");
+
+        int pid = fork();
+        if (pid == 0) { // Child process
+            close(listen_sd);
+            handle_client(client_fd, ctx);
+            exit(0);
+        } else if (pid > 0) { // Parent process
+            close(client_fd);
+        } else {
+            perror("Fork failed");
+            exit(1);
+        }
+    }
+
+    close (listen_sd);
+    SSL_CTX_free (ctx); // Release context
 }
-/* EOF - serv.cpp */
+
+void handle_client(int sd, SSL_CTX* ctx) {
+    SSL* ssl;
+    ssl = SSL_new (ctx);                           CHK_NULL(ssl);
+    SSL_set_fd (ssl, sd);
+    int err = SSL_accept (ssl);                    CHK_SSL(err);
+
+    printf ("SSL connection using %s\n", SSL_get_cipher (ssl));
+
+    X509* client_cert = SSL_get_peer_certificate (ssl);
+    if (client_cert != NULL) {
+        char* str = X509_NAME_oneline (X509_get_subject_name (client_cert), 0, 0);
+        CHK_NULL(str);
+        printf ("\t subject: %s\n", str);
+        OPENSSL_free (str);
+
+        str = X509_NAME_oneline (X509_get_issuer_name (client_cert), 0, 0);
+        CHK_NULL(str);
+        printf ("\t issuer: %s\n", str);
+        OPENSSL_free (str);
+
+        X509_free (client_cert);
+    } else {
+        printf ("Client does not have certificate.\n");
+    }
+
+    char buf[4096];
+    int read = SSL_read (ssl, buf, sizeof(buf) - 1);    CHK_SSL(err);
+    buf[read] = '\0';
+    printf ("Got %d chars:'%s'\n", read, buf);
+    // Prepare a message with the size of the received message
+    char message[1024];
+//    sprintf(message, "Received %d bytes: %s", read, buf);
+    sprintf(message, "%d[b]", read);
+
+    /*
+     * Pro kontrolu by bylo vhodné, aby server klientovi zpět potvrdil, kolik bajtů přijat.
+     */
+    err = SSL_write (ssl, message, strlen(message));  CHK_SSL(err);
+
+    close(sd);
+    SSL_free(ssl);
+}
+
+
